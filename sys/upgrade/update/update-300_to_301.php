22be81962639688ef15b299d76f33a19bd82d4f5
ua.lifesheets <ua.lifesheets@gmail.com>
2024-01-27 05:22:27 +0200

    Minor repository settings.

--- sys/upgrade/update/update-300_to_301.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-if (is_file(ROOTPATH . '/pages/chat.php')) {
-	unlink(ROOTPATH . '/pages/chat.php'); 
-}
-
-db::query("ALTER TABLE `mail_contacts` ADD `time_update` INT(11) NOT NULL AFTER `status`"); 
